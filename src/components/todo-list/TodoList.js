eee91a6a5813314ec639d9ba1c105185bbe90016
luthfyhakim <luthfyhakim250404@gmail.com>
2022-05-13 21:23:19 +0700

    add component child todo-list

---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,19 @@
+import './TodoList.css'
+
+const TodoList = (props) => {
+    console.log(props.dataTodos);
+    return (
+        <ul>
+            {
+                props.dataTodos.map((todo) => {
+                    // {id: 1, title: 'Eat'}
+                    return (
+                        <li key={todo.id}>{todo.title}</li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+export default TodoList
